bd6221ad42b72b4b9bc3632303c76828d9087230
julia-jung <minji6654@gmail.com>
2024-09-17 21:57:49 +0900

    test: Fix failed test for PropertyListing

--- src/components/PropertyListing/tests/PropertyListing.spec.jsx
+++ src/components/PropertyListing/tests/PropertyListing.spec.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { within } from '@testing-library/dom';
+import { http, HttpResponse } from 'msw';
+import { setupServer } from 'msw/node';
 import PropertyListing from '../PropertyListing';
 
 describe('PropertyListing', () => {
-    it('should render five property cards', async () => {
+    const DUMMY_PROPERTY = {
+        id: 73864112,
+        bedrooms: 3,
+        summary: 'Property 1 Situated moments from the River Thames in Old Chelsea...',
+        displayAddress: '1 CHEYNE WALK, CHELSEA, SW3',
+        propertyType: 'Flat',
+        price: 1950000,
+        branchName: 'M2 Property, London',
+        propertyUrl: '/property-for-sale/property-73864112.html',
+        contactUrl: '/property-for-sale/contactBranch.html?propertyId=73864112',
+        propertyTitle: '3 bedroom flat for sale',
+        mainImage:
+            'https://media.rightmove.co.uk/dir/crop/10:9-16:9/38k/37655/53588679/37655_CAM170036_IMG_01_0000_max_476x317.jpg',
+    };
+    const server = setupServer(
+        http.get('http://localhost:3000/api/properties', () => {
+            return HttpResponse.json(
+                Array(5)
+                    .fill(DUMMY_PROPERTY)
+                    .map((property, index) => ({ ...property, id: `${property.id}_${index}` }))
+            );
+        })
+    );
+    beforeAll(() => server.listen());
+    afterEach(() => server.resetHandlers());
+    afterAll(() => server.close());
+
+    it('should render property cards for fetched properties', async () => {
         render(<PropertyListing />);
         const propertiesList = screen.getByRole('list');
         const propertyCards = await within(propertiesList).findAllByRole('listitem');
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,11 @@ import '@testing-library/jest-dom/extend-expect';
 
 // Polyfill fetch support on Node
 import 'cross-fetch/polyfill';
+
+// polyfills for Node.js globals
+const { TextDecoder, TextEncoder } = require('node:util');
+
+Object.defineProperties(globalThis, {
+    TextDecoder: { value: TextDecoder },
+    TextEncoder: { value: TextEncoder },
+});
